b4868558bb31018c0f0cb81d0d2b624094b102ef
Kynako <Kynako@users.noreply.github.com>
2022-02-13 22:38:15 +0900

    fix(logger): fixed typo

--- logger.js
+++ logger.js
@@ -14,8 +14,8 @@
  * @package
  */
 const ERR_MSGS = {
-  INVALID_TYPE: (name, expected, an=false) => `Supplied ${name} is not ${an ? 'n' : ''} ${expected}.`,
-  TIMER_NOT_EXIST: label => `Timer '${label} doesn't exst.`
+  INVALID_TYPE: (name, expected, an=false) => `Supplied ${name} is not ${an ? 'an' : 'a'} ${expected}.`,
+  TIMER_NOT_EXIST: label => `Timer '${label} doesn't exist.`
 };
 
 /**
