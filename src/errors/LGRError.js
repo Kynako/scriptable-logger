f9be03c76ff50f265a9125b13be9dc53003a94e3
Kynako <Kynako@users.noreply.github.com>
2022-02-15 08:48:55 +0900

    feat(errors): added JSDoc at src/errors/LGRError.js

--- src/errors/LGRError.js
+++ src/errors/LGRError.js
@@ -1,11 +1,21 @@
-module.exports = class LGRError extends Error {
+/**
+ * LGRError
+ * @class
+ * @extends Error
+ * @param {any} msg
+ */
+class LGRError extends Error {
   
   /**
    * constructor(msg)
+   * @constructor
    * @param {String} msg
+   * @return {LRGError}
    */
   constructor(msg) {
     super(msg);
     this.name = this.constructor.name;
   };
 };
+
+module.exports = LGRError;
